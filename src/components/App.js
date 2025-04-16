2f1c462c0cd7f5cc8eecd58719b1bd6fec6c4fce
Sowda <ayanmoha4949@gmail.com>
2025-04-16 07:31:46 -0700

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
+
 import React from "react";
-import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
-
 function App() {
+  const user = {
+    name: "Ayan Mohamed",
+    bio: "Frontend developer diving into backend development.",
+    github: "https://github.com/sowdaahmed",
+    linkedin: "https://www.linkedin.com/in/ayanmohamed"
+  };
+
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} />
+      <About bio={user.bio} github={user.github} linkedin={user.linkedin} />
     </div>
   );
 }
